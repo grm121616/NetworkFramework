696f66e7c86836505313f917983e89070cb6a3d0
Jerry <niuBchen@hotmail.com>
2020-06-07 17:34:16 -0400

    first commit

---
+++ NetworkFramework/NetworkFramework.h
@@ -0,0 +1,19 @@
+//
+//  NetworkFramework.h
+//  NetworkFramework
+//
+//  Created by Ruoming Gao on 6/7/20.
+//  Copyright © 2020 Ruoming Gao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkFramework.
+FOUNDATION_EXPORT double NetworkFrameworkVersionNumber;
+
+//! Project version string for NetworkFramework.
+FOUNDATION_EXPORT const unsigned char NetworkFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkFramework/PublicHeader.h>
+
+
